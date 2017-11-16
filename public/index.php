e392da8b3b0072548808a2fb1544783da7a5ccb7
Hurricane8 <paradox-sales19@live.fr>
2017-11-16 11:03:37 +0100

    implement quest wcs

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require "../vendor/autoload.php";
+require "../src/model/QuestManager.php";
+require "../connect.php";
+
+$req = new \project\model\QuestManager();
+$res = $req->findAll();
---
+++ src/model/Quest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace project\model;
+
+
+class Quest
+{
+    private $id;
+    private $name;
+    private $course;
+    private $score;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Quest
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Quest
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCourse()
+    {
+        return $this->course;
+    }
+
+    /**
+     * @param mixed $course
+     * @return Quest
+     */
+    public function setCourse($course)
+    {
+        $this->course = $course;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param mixed $score
+     * @return Quest
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+        return $this;
+    }
+}
